2a23d20ad34316323957ceda485044453fe00594
Colin <SirNiloc@users.noreply.github.com>
2019-02-06 00:11:15 -0600

    Update build.gradle

--- src/main/java/com/sirniloc/yam/character/capa/CapabilityABS.java
+++ src/main/java/com/sirniloc/yam/character/capa/CapabilityABS.java
@@ -4,9 +4,7 @@ import com.sirniloc.yam.character.capa.interfaces.IAbilityScores;
 
 import net.minecraftforge.common.capabilities.CapabilityManager;
 
-public class CapabilityABS {
-
-	
+public class CapabilityABS {	
 	public static void postInit()
 	{
 		CapabilityManager.INSTANCE.register(IAbilityScores.class, new ABSStorage(), new ABSStorage.Factory());
