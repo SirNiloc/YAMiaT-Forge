93f3bbad24d9286bdce30726690ce5d672224ee2
Colin <SirNiloc@users.noreply.github.com>
2019-02-06 22:30:09 -0600

    Update YAM.java

--- src/main/java/com/sirniloc/yam/YAM.java
+++ src/main/java/com/sirniloc/yam/YAM.java
@@ -16,7 +16,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 public class YAM
 {
     public static final String MODID = "mdk";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.2";
     
     
     @CapabilityInject(IAbilityScores.class)
