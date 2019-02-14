c440bc5f8fbf33be0f42a9482e90d3b39940e507
Colin <SirNiloc@users.noreply.github.com>
2019-02-14 12:37:45 -0600

    Start v0.5

--- src/main/java/com/sirniloc/yam/BaseYAM.java
+++ src/main/java/com/sirniloc/yam/BaseYAM.java
@@ -17,7 +17,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 public class BaseYAM
 {
     public static final String MODID = "yam";
-    public static final String VERSION = "0.4";
+    public static final String VERSION = "0.5";
     
     
     @CapabilityInject(IYam.class)
